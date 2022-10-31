299c7b4bee017490a0104baa86ac823dbb28d523
Yago <yagopalmas@gmail.com>
2022-11-01 00:48:00 +0100

    ponemos comentarios en el codigo

--- createvm_cli.sh
+++ createvm_cli.sh
@@ -2,7 +2,7 @@
 
 set -u
 
-
+# Comprobamos si hay subscripcion activa por defecto
 if az account show|grep "state.: .Enabled" > /dev/null;then
 	echo "Estas conectado con subscripcion activa"
 	 az account show
@@ -12,7 +12,7 @@ else
 	exit 1
 fi
 
-
+# Pedimos el grupo de Recursos
 echo "Vamos crear un grupo de recursos en East US:"
 read -p "dame el nombre del grupo de recursos:" NAMERG
 
@@ -22,6 +22,7 @@ if [ $? -ne 0 ];then
 	exit 2
 fi
 
+# Creamos la red virtual
 echo "Creamos red virtual en el grupo de recurso $NAMERG con subred 192.168.10.0/24"
 echo "az network vnet create -g $NAMERG -n myvn1 --address-prefix 192.168.0.0/16 --subnet-name MySubnet --subnet-prefix 192.168.10.0/24"
 az network vnet create -g $NAMERG -n myvn1 --address-prefix 192.168.0.0/16 --subnet-name MySubnet --subnet-prefix 192.168.10.0/24
@@ -30,6 +31,7 @@ if [ $? -ne 0 ];then
 	exit 3
 fi
 
+# Creamos maquina virtual
 echo "Creamos maquina virtual en el grupo de recurso $NAMERG en la subred 192.168.10.0/24"
 echo "az vm create -n MyVm -g  $NAMERG --image Centos --subnet MySubnet --vnet-name myvn1  --data-disk-sizes-gb 10 --size Standard_DS1_v2   --admin-username yagovm  --ssh-key-values /home/yago/.ssh/id_rsa.pub --public-ip-sku Standard"
 az vm create -n MyVm -g  $NAMERG --image Centos --subnet MySubnet --vnet-name myvn1  --data-disk-sizes-gb 10 --size Standard_DS1_v2   --admin-username yagovm  --ssh-key-values /home/yago/.ssh/id_rsa.pub --public-ip-sku Standard > /tmp/vm_create.json
@@ -45,6 +47,7 @@ echo "Probamos a conectarnos por ssh"
 IP_PUBLICA=$(cat /tmp/vm_create.json |grep publicIpAddress|cut -d\: -f2|grep -Eo "[0-9.]*")
 
 
+#Comprobamos que hay conexion ssh sacando el hostname
 sleep 5
 echo "Comprobamos que tenemos conexion ssh con la IP publica $IP_PUBLICA"
 ssh -p22  -o StrictHostKeyChecking=no -i /home/yago/.ssh/id_rsa yagovm@$IP_PUBLICA  "hostname -f"
